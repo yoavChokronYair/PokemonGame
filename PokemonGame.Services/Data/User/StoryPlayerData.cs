022e158096ada1a156753777f72ca1eae28b3179
yoavChokronYair <yoavyair2008@gmail.com>
2025-10-27 19:32:07 +0200

    user data

---
+++ PokemonGame.Services/Data/User/StoryPlayerData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonGame.Services.Data.User
+{
+    public class StoryPlayerData : UserData
+    {
+        private int name;
+        private int pID;
+        public int Name { get => name; set => name = value; }
+        public int PID { get => pID; set => pID = value; }
+    }
+}
---
+++ PokemonGame.Services/Data/User/UserData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokemonGame.Services.Data.User
+{
+    public class UserData
+    {
+        private int userID;
+        private string userName;
+        private int password;
+        public int UserID { get => userID; set => userID = value; }
+        public string UserName { get => userName; set => userName = value; }
+        public int Password { get => password; set => password = value; }
+    }
+}
