9b35b1cd04b1d3515e1eafd78bfe82c3c9af16da
yoavChokronYair <153995316+yoavChokronYair@users.noreply.github.com>
2025-11-26 08:33:09 +0200

    old working code

--- PokemonGame/Views/Controls/BoardBackground.cs
+++ PokemonGame/Views/Controls/BoardBackground.cs
@@ -3,37 +3,30 @@ using System.Windows.Controls;
 
 namespace PokemonGame.Views.Controls
 {
-    // Moved enum OUTSIDE the class to avoid naming conflicts
     public enum BackgroundType
     {
         White,
         Blue,
         Red
     }
-
     public class BoardBackground : ContentControl
     {
-        // Define the dependency property with the correct enum type
+
         public static readonly DependencyProperty BackgroundTypeProperty =
-            DependencyProperty.Register(
-                nameof(BackgroundType),
-                typeof(BackgroundType),  // This now correctly references the enum
-                typeof(BoardBackground),
+            DependencyProperty.Register(nameof(BackgroundType), typeof(BackgroundType), typeof(BoardBackground),
                 new PropertyMetadata(BackgroundType.White));
 
-        // Property accessor
         public BackgroundType BackgroundType
         {
             get => (BackgroundType)GetValue(BackgroundTypeProperty);
             set => SetValue(BackgroundTypeProperty, value);
         }
 
-        // Static constructor to set the default style
         static BoardBackground()
         {
-            DefaultStyleKeyProperty.OverrideMetadata(
-                typeof(BoardBackground),
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BoardBackground),
                 new FrameworkPropertyMetadata(typeof(BoardBackground)));
         }
+
     }
 }
