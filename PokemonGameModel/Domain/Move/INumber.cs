9086a1352bf779d127a7c1180f7194e3260ae41f
yoavChokronYair <yoavyair2008@gmail.com>
2026-03-11 04:40:29 +0200

    created the basic idea of INumber and how it should work will probably need to expand,it will not be infinite amout which makes it possible

--- PokemonGameModel/Domain/Move/INumber.cs
+++ PokemonGameModel/Domain/Move/INumber.cs
@@ -1,6 +1,79 @@
-﻿namespace PokemonGame.Model.Domain.Move
+﻿using PokemonGame.Model.Domain.Battle;
+
+namespace PokemonGame.Model.Domain.Move
 {
+    //probabely use a deleget
     public interface INumber
     {
+        //combinators:+/6
+
+        //product,sum,quotient 
+        public double Evaluate(BattleDomain battle);
+    }
+    public class Exactly : INumber
+    {
+        public double exactDamaged;
+
+        public double Evaluate(BattleDomain battle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Between : INumber
+    {
+        public double min;
+        public double max;
+
+        public double Evaluate(BattleDomain battle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Weighted : INumber
+    {
+        //ToDO:understand more
+        public List<double> weighted;
+
+        public double Evaluate(BattleDomain battle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    //can be not by a battle but a battler
+    public class MaxHP: INumber
+    {
+        public ITarget target;
+
+        public double Evaluate(BattleDomain battle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class CuerrentHP : INumber
+    {
+        public ITarget target;
+
+        public double Evaluate(BattleDomain battle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Level : INumber
+    {
+        public ITarget target;
+
+        public double Evaluate(BattleDomain battle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class LastDamageDealt : INumber
+    {
+        public ITarget target;
+
+        public double Evaluate(BattleDomain battle)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
