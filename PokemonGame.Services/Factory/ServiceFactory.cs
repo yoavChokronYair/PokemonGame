dc7231e0b3db704fc52f0346478d3fdb0ab15ce5
yoavChokronYair <yoavyair2008@gmail.com>
2026-03-16 17:27:36 +0200

    factory updated

--- PokemonGame.Services/Factory/ServiceFactory.cs
+++ PokemonGame.Services/Factory/ServiceFactory.cs
@@ -8,18 +8,55 @@ namespace PokemonGame.Services.Factory
         private static readonly Lazy<ServiceFactory> _instance = new(() => new ServiceFactory());
         public static ServiceFactory Instance => _instance.Value;
 
+        // Player & Auth
         internal SQLiteUserRepository UserRepository { get; }
         internal SQLiteOnlinePlayerRepository OnlinePlayerRepository { get; }
+
+        // Social
+        internal SQLiteFriendRepository FriendRepository { get; }
+
+        // Teams & Battling
+        internal SQLiteTeamRepository TeamRepository { get; }
+        internal SQLiteTeamMemberRepository TeamMemberRepository { get; }
+        internal SQLiteBattleRepository BattleRepository { get; }
+        internal SQLiteParticipantRepository ParticipantRepository { get; }
+
+        // Pokemon & Moves
+        internal SQLitePokemonRepository PokemonRepository { get; }
+        internal SQLiteBattlerPokemonRepository BattlerPokemonRepository { get; }
         internal SQLiteMoveRepository MoveRepository { get; }
+
+        // Static Lookups (Cached)
+        internal SQLiteAbilityRepository AbilityRepository { get; }
+        internal SQLiteItemRepository ItemRepository { get; }
+        internal SQLiteBreedingRepository BreedingRepository { get; }
+        internal SQLitePokedexEntryRepository PokedexEntryRepository { get; }
+        internal SQLiteMoveLearnsetRepository MoveLearnsetRepository { get; }
 
         private ServiceFactory()
         {
             var db = new SQLiteConnectionService(
                 "C:\\Users\\yoav\\Source\\Repos\\PokemonGame\\PokemonGame.Services\\resources\\DB\\PokemonGameDB.db");
 
+            // Initialize all repositories with the shared connection service
             UserRepository = new SQLiteUserRepository(db);
             OnlinePlayerRepository = new SQLiteOnlinePlayerRepository(db);
+            FriendRepository = new SQLiteFriendRepository(db);
+
+            TeamRepository = new SQLiteTeamRepository(db);
+            TeamMemberRepository = new SQLiteTeamMemberRepository(db);
+            BattleRepository = new SQLiteBattleRepository(db);
+            ParticipantRepository = new SQLiteParticipantRepository(db);
+
+            PokemonRepository = new SQLitePokemonRepository(db);
+            BattlerPokemonRepository = new SQLiteBattlerPokemonRepository(db);
             MoveRepository = new SQLiteMoveRepository(db);
+
+            AbilityRepository = new SQLiteAbilityRepository(db);
+            ItemRepository = new SQLiteItemRepository(db);
+            BreedingRepository = new SQLiteBreedingRepository(db);
+            PokedexEntryRepository = new SQLitePokedexEntryRepository(db);
+            MoveLearnsetRepository = new SQLiteMoveLearnsetRepository(db);
         }
     }
 }
