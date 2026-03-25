5f3f86c5fd7bc2d491b13a2625e463d3c0c62946
yoavChokronYair <yoavyair2008@gmail.com>
2026-03-25 20:46:12 +0200

    cleaned up old code and added plain ui for pokemon battling

---
+++ PokemonGame.ViewModels/ViewModelPage/BattleMenu/BattleMenuViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+using PokemonGame.ViewModels.ViewModelHelper;
+
+namespace PokemonGame.ViewModels.ViewModelPage.BattleMenu
+{
+    public class BattleMenuViewModel : ViewModelBase
+    {
+        private bool _isMovesetVisible;
+
+        public bool IsMovesetVisible
+        {
+            get => _isMovesetVisible;
+            set
+            {
+                if (SetProperty(ref _isMovesetVisible, value))
+                {
+                    // When this changes, the MainMenu visibility must also update
+                    OnPropertyChanged(nameof(IsMainMenuVisible));
+                }
+            }
+        }
+
+        // This is the "Inverse" property for your Converter
+        public bool IsMainMenuVisible => !IsMovesetVisible;
+
+        public ICommand OpenMovesetCommand { get; }
+        public ICommand CloseMovesetCommand { get; }
+
+        public BattleMenuViewModel()
+        {
+            IsMovesetVisible = false;
+            OpenMovesetCommand = new RelayCommand(() => IsMovesetVisible = true);
+            CloseMovesetCommand = new RelayCommand(() => IsMovesetVisible = false);
+        }
+    }
+}
---
+++ PokemonGame.ViewModels/ViewModelPage/BattleMenu/EnemyBattleStatusViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System.Windows.Media;
+using PokemonGame.ViewModels.ViewModelHelper;
+
+namespace PokemonGame.ViewModels.ViewModelPage.BattleMenu
+{
+    public class EnemyBattleStatusViewModel : ViewModelBase
+    {
+        private string _pokemonName = "FOE PIKACHU";
+        private int _level = 20;
+        private int _currentHp = 50;
+        private int _maxHp = 50;
+        private string _gender = "Female"; // Options: "Male", "Female", "None"
+
+        public string PokemonName
+        {
+            get => _pokemonName;
+            set => SetProperty(ref _pokemonName, value);
+        }
+
+        public int Level
+        {
+            get => _level;
+            set => SetProperty(ref _level, value);
+        }
+
+        public int CurrentHP
+        {
+            get => _currentHp;
+            set
+            {
+                if (SetProperty(ref _currentHp, value))
+                {
+                    OnPropertyChanged(nameof(HpPercentage));
+                    OnPropertyChanged(nameof(HPColor));
+                }
+            }
+        }
+
+        public int MaxHP
+        {
+            get => _maxHp;
+            set
+            {
+                if (SetProperty(ref _maxHp, value))
+                {
+                    OnPropertyChanged(nameof(HpPercentage));
+                    OnPropertyChanged(nameof(HPColor));
+                }
+            }
+        }
+
+        public string Gender
+        {
+            get => _gender;
+            set
+            {
+                if (SetProperty(ref _gender, value))
+                {
+                    OnPropertyChanged(nameof(GenderSymbol));
+                    OnPropertyChanged(nameof(GenderColor));
+                }
+            }
+        }
+
+        // --- Logic Helper Properties ---
+
+        // Returns ratio (0.0 to 1.0) for the ScaleTransform
+        public double HpPercentage => MaxHP > 0 ? (double)CurrentHP / MaxHP : 0;
+
+        // Automatically changes the color of the bar based on health percentage
+        public Brush HPColor
+        {
+            get
+            {
+                double ratio = HpPercentage;
+                if (ratio > 0.5) return new SolidColorBrush(Color.FromRgb(80, 240, 144));  // Green
+                if (ratio > 0.2) return new SolidColorBrush(Color.FromRgb(248, 224, 56)); // Yellow
+                return new SolidColorBrush(Color.FromRgb(248, 88, 56));                  // Red
+            }
+        }
+
+        // Returns symbol based on string value
+        public string GenderSymbol => Gender == "Male" ? "♂" : (Gender == "Female" ? "♀" : "");
+
+        // Returns blue for male, pink for female
+        public Brush GenderColor => Gender == "Male" ? Brushes.DeepSkyBlue : Brushes.HotPink;
+    }
+}
