f3cdbc98388a23f3fc914e84b2e3ff7b9f974005
yoavChokronYair <yoavyair2008@gmail.com>
2026-04-22 15:28:54 +0300

    foreground layer and background layer

--- PokemonGameModel/Domain/Map/TileDomain.cs
+++ PokemonGameModel/Domain/Map/TileDomain.cs
@@ -1,4 +1,5 @@
-﻿using PokemonGame.Model.Enums;
+﻿using System.Runtime.CompilerServices;
+using PokemonGame.Model.Enums;
 
 namespace PokemonGame.Model.Domain.Map
 {
@@ -16,6 +17,7 @@ namespace PokemonGame.Model.Domain.Map
     public class MapDomain
     {
         public string Name { get; set; }
+        public List<TileDomain> BackgroundBlocks {  get; set; }
         public List<TileDomain> Blocks {  get; set; }
         public int Width { get; set; }
         public int Height { get; set; }
@@ -33,30 +35,43 @@ namespace PokemonGame.Model.Domain.Map
         public ConnectionDirection ConnectionDirection { get; set; }
         public int Margin { get; set; }
     }
+    public class PlayerState
+    {
+        public int LocationRow { get; set; }
+        public int LocationCol { get; set; }
+        public FacingDirection facingDirection { get; set; }
 
+    }
 
     public class MapState
     {
-
         private MapDomain _activeMap;
-        private int[,] _blockTiles;
-        private readonly Dictionary<MapDomain, int[,]> _mapCache = new();
+        private int[,] _backgroundTiles;
+        private int[,] _foregroundTiles;//has collision layer info, used for battle and movement
+        private readonly Dictionary<MapDomain, (int[,] bg, int[,] fg)> _mapCache = new();
 
         public MapState(MapDomain startMap)
         {
             _activeMap = startMap;
-            _blockTiles = GetCachedTiles(startMap);
+            (_backgroundTiles, _foregroundTiles) = GetCachedTiles(startMap);
         }
 
-        private int[,] GetCachedTiles(MapDomain map)
+        private (int[,] bg, int[,] fg) GetCachedTiles(MapDomain map)
         {
             if (_mapCache.TryGetValue(map, out var cached)) return cached;
-            var built = BuildTileArray(map);
+            var built = (BuildTileArray(map.BackgroundBlocks, map), BuildTileArray(map.Blocks, map));
             _mapCache[map] = built;
             return built;
         }
 
-        public int[,] BuildViewport((int Row, int Col) playerPos)
+        public (int[,] background, int[,] foreground) BuildViewPort((int playerRow, int playerCol) playerPos)
+        {
+            var bg = BuildLayerViewport(playerPos, _backgroundTiles);
+            var fg = BuildLayerViewport(playerPos, _foregroundTiles);
+            return (bg, fg);
+        }
+        
+        private static int[,] BuildLayerViewport((int playerRow, int playerCol) playerPos, int[,] layer)
         {
             int halfRows = MapConstants.ViewRowSize / 2;
             int halfCols = MapConstants.ViewColSize / 2;
@@ -67,27 +82,26 @@ namespace PokemonGame.Model.Domain.Map
             {
                 for (int c = 0; c < MapConstants.ViewColSize; c++)
                 {
-                    int srcRow = playerPos.Row - halfRows + r;
-                    int srcCol = playerPos.Col - halfCols + c;
+                    int srcRow = playerPos.playerRow - halfRows + r;
+                    int srcCol = playerPos.playerCol - halfCols + c;
 
                     view[r, c] =
-                        (uint)srcRow < (uint)_blockTiles.GetLength(0) && (uint)srcCol < (uint)_blockTiles.GetLength(1)
-                            ? _blockTiles[srcRow, srcCol]
-                            : _blockTiles[srcRow, srcCol];
+                        (uint)srcRow < (uint)layer.GetLength(0) && (uint)srcCol < (uint)layer.GetLength(1)
+                            ? layer[srcRow, srcCol]
+                            : 0;
                 }
             }
+
             return view;
         }
 
-        private static int[,] BuildTileArray(MapDomain map)
+        private static int[,] BuildTileArray(List<TileDomain> blocks, MapDomain map)
         {
-            int rows = map.Height * MapConstants.BlockSize;
-            int cols = map.Width * MapConstants.BlockSize;
-            var tiles = new int[rows, cols];
+            var tiles = new int[map.Height * MapConstants.BlockSize, map.Width * MapConstants.BlockSize];
 
-            for (int b = 0; b < map.Blocks.Count; b++)
+            for (int b = 0; b < blocks.Count; b++)
             {
-                var tile = map.Blocks[b];
+                var tile = blocks[b];
                 if (tile is null) continue;
 
                 tiles[b / map.Width, b % map.Width] = tile.Tileid;
