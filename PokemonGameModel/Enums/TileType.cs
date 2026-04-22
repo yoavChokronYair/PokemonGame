485c3a56fe7bccb64f2a10a2d0d6c70766e3b07d
yoavChokronYair <yoavyair2008@gmail.com>
2026-04-22 15:43:11 +0300

    neigboher map tiles

--- PokemonGameModel/Enums/TileType.cs
+++ PokemonGameModel/Enums/TileType.cs
@@ -4,7 +4,7 @@ namespace PokemonGame.Model.Enums
 {
     public enum TileType { None = 0, Water = 1, Npc = 2, TallGrass = 3 }
     public enum MapTilesType { Club = 0, Overworld = 1 }
-    public enum ConnectionDirection { Up, Down, Left, Right }
+    public enum ConnectionDirection { North, South, East, West }
     public enum FacingDirection { Up, Down, Left, Right }
 
 }
