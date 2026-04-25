0fadb6018ca789a9ce235ca7ed194ed44d6564b5
yoavChokronYair <yoavyair2008@gmail.com>
2026-04-25 09:46:22 +0300

    map file seperation

---
+++ PokemonGame.ViewModels/ViewModelPage/Map/Command/MoveCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PokemonGame.Model.Enums;
+using PokemonGame.ViewModels.ViewModelHelper;
+
+namespace PokemonGame.ViewModels.ViewModelPage.Map.Command
+{
+    public class MoveCommand : CommandBase
+    {
+        private readonly MapViewModel _vm;
+        private readonly FacingDirection _direction;
+
+        public MoveCommand(MapViewModel vm, FacingDirection direction)
+        {
+            _vm = vm;
+            _direction = direction;
+        }
+
+        public override void Execute(object? parameter) => _vm.Move(_direction);
+    }
+}
