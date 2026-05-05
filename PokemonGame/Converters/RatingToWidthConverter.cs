5acda648a517e17c40a81233fe0f0ef7f4c4a2e0
yoavChokronYair <153995316+yoavChokronYair@users.noreply.github.com>
2026-05-05 20:29:59 +0300

    basic ui fot after game

---
+++ PokemonGame/Converters/RatingToWidthConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PokemonGame.Converters
+{
+    public class RatingToWidthConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length < 2) return 0d;
+            if (!double.TryParse(values[0]?.ToString(), out var current)) return 0d;
+            if (!double.TryParse(values[1]?.ToString(), out var max) || max <= 0) return 0d;
+
+            double barWidth = parameter is string p && double.TryParse(p, out var pw) ? pw : 200d;
+            return Math.Clamp(current / max * barWidth, 0, barWidth);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
